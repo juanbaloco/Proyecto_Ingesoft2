e2e6b91ca1ff058cebdde9f071ce4eb1040f3db1
Juano-LP <juanchomontealeg@gmail.com>
2025-10-17 20:11:16 -0500

    Tests verificados

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,5 @@
 import '@testing-library/jest-dom' // Importa las utilidades de jest-dom
+import { TextEncoder, TextDecoder } from 'util';
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
